be0cb86ea5cad7864d8657261796e0eba2b0afe6
Will Moore <william@webs.com>
2015-05-29 16:55:42 -0400

    Rewrite for cleanliness
    Added time formatting too

--- src/main/groovy/com/webs/gradle/GitPropertiesPlugin.groovy
+++ src/main/groovy/com/webs/gradle/GitPropertiesPlugin.groovy
@@ -8,6 +8,8 @@ import org.gradle.api.Task
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.TaskAction
+import org.joda.time.DateTimeZone
+import org.joda.time.format.ISODateTimeFormat
 
 class GitPropertiesPlugin implements Plugin<Project> {
 	@Override
@@ -24,26 +26,27 @@ class GitPropertiesPlugin implements Plugin<Project> {
 	static class GitPropertiesTask extends DefaultTask {
 		@TaskAction
 		void generate() {
+			// note: deprecated, but the examples still use this syntax.  Not sure what to do here?
 			def repo = Grgit.open(project.file('.'))
 			def dir = new File(project.buildDir, "resources/main")
-			def file = new File(project.buildDir, "resources/main/git.properties")
-			if (!dir.exists()) {
-				dir.mkdirs()
+			if (!dir.exists()) dir.mkdirs()
+
+			def dateTimeFormat = ISODateTimeFormat.basicDateTimeNoMillis().withZone DateTimeZone.UTC
+
+			new File(dir, "git.properties").withWriter { w ->
+				def props = new Properties()
+				props.putAll([
+						"git.branch"				: repo.branch.current.name,
+						"git.commit.id"				: repo.head().id,
+						"git.commit.id.abbrev"		: repo.head().abbreviatedId,
+						"git.commit.user.name"		: repo.head().author.name,
+						"git.commit.user.email"		: repo.head().author.email,
+						"git.commit.message.short"	: repo.head().shortMessage,
+						"git.commit.message.full"	: repo.head().fullMessage,
+						"git.commit.time"			: dateTimeFormat.print(new Long(repo.head().time)*1000)
+				])
+				props.store w, 'Git properties (written at build time by Git-Properties-Plugin'
 			}
-			if (!file.exists()) {
-				file.createNewFile()
-			}
-			def map = ["git.branch"                : repo.branch.current.name
-					   , "git.commit.id"           : repo.head().id
-					   , "git.commit.id.abbrev"    : repo.head().abbreviatedId
-					   , "git.commit.user.name"    : repo.head().author.name
-					   , "git.commit.user.email"   : repo.head().author.email
-					   , "git.commit.message.short": repo.head().shortMessage
-					   , "git.commit.message.full" : repo.head().fullMessage
-					   , "git.commit.time"         : repo.head().time.toString()]
-			def props = new Properties()
-			props.putAll(map)
-			props.store(file.newWriter(), "")
 		}
 	}
 }
